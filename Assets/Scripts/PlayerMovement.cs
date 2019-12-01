8cca2e8a525b4de00e47c26c85be9ae03d7413d3
Matthew Phillips <Matt1333985@gmail.com>
2019-11-30 20:12:10 -0500

    Added basic sprites and scripts into the game

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rig;
+
+    public float speed = 1;
+
+    private bool facingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    //called a fixed amount of times, use for physics
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+
+        rig.velocity = new Vector2(moveInput * speed, rig.velocity.y);
+
+        //checks to see if player is facing the right way based on move direction
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+
+    //flips the sprite to face the way the character is moving
+    void Flip()
+    {
+
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+
+    }
+
+}
