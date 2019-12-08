a7fbe0e7c8ad060a40097e1f6948330d36ff14d8
Matthew Phillips <Matt1333985@gmail.com>
2019-12-07 21:28:30 -0500

    updated Unity version while adding another piece

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    Vector2 startPos = new Vector2(0,0);
+    public GameObject roboPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.name == "Robo")
+        {
+
+            Transform roboPos = roboPrefab.GetComponent<Transform>();
+
+            roboPos.position = startPos;
+
+        }
+    }
+}
